c412b002cd9a1514a8b377ac4e363d1d6f2a4649
rech <rjseguido@gmail.com>
2022-08-31 16:55:13 +0800

    remove gif

--- lib/src/domain/providers/notifiers/draggable_widget_notifier.dart
+++ lib/src/domain/providers/notifiers/draggable_widget_notifier.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:modal_gif_picker/modal_gif_picker.dart';
 import 'package:stories_editor/src/domain/models/editable_items.dart';
 
 class DraggableWidgetNotifier extends ChangeNotifier {
